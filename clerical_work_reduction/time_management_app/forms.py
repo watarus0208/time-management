e50c011aaef69808a441e78fe725fa969fe9c049
杉本亙 <sugimotowataru@sugimotowatarunoMacBook-Air.local>
2022-11-28 20:01:09 +0900

    月次ページ・モデル修正

--- clerical_work_reduction/time_management_app/forms.py
+++ clerical_work_reduction/time_management_app/forms.py
@@ -25,3 +25,14 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username',)
+
+
+class MonthForm(forms.Form):
+    start_time = forms.TimeField()
+    close_time = forms.TimeField()
+    attendance_time = forms.TimeField()
+    work_time = forms.TimeField()
+    break_time = forms.TimeField()
+    overtime_break_time = forms.TimeField()
+    today_plan = forms.Textarea()
+    today_issue = forms.Textarea()
--- clerical_work_reduction/time_management_app/models.py
+++ clerical_work_reduction/time_management_app/models.py
@@ -2,52 +2,52 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    address = models.CharField(max_length=100,blank=True)
+    address = models.CharField(max_length=100,blank=True, verbose_name='住所')
     
 
 class ContractCompanies(models.Model):
-    name = models.CharField(max_length=50,blank=True)
-    address = models.CharField(max_length=50,blank=True)
-    zip_code = models.CharField(max_length=8,blank=True)
-    president = models.CharField(max_length=20,blank=True)
-    phone_number = models.CharField(max_length=12,blank=True)
+    name = models.CharField(max_length=50,blank=True, verbose_name='会社名')
+    address = models.CharField(max_length=50,blank=True, verbose_name='会社住所')
+    zip_code = models.CharField(max_length=8,blank=True, verbose_name='郵便番号')
+    president = models.CharField(max_length=20,blank=True, verbose_name='代表名')
+    phone_number = models.CharField(max_length=12,blank=True, verbose_name='電話番号')
 
 
 class SalesPeople(models.Model):
-    first_name = models.CharField(max_length=10)
-    last_name = models.CharField(max_length=10)
-    email = models.EmailField(max_length=200,blank=True)
-    mobile_phone_number = models.CharField(max_length=12,blank=True)
+    first_name = models.CharField(max_length=10, verbose_name='名')
+    last_name = models.CharField(max_length=10, verbose_name='氏')
+    email = models.EmailField(max_length=200,blank=True, verbose_name='Eメール')
+    mobile_phone_number = models.CharField(max_length=12,blank=True, verbose_name='携帯電話番号')
     user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='ユーザー')
     company = models.ForeignKey(ContractCompanies, on_delete=models.PROTECT, verbose_name='契約会社')
 
 
 class Projects(models.Model):
-    project_name = models.CharField(max_length=100)
-    start_month = models.CharField(max_length=2)
-    last_month = models.CharField(max_length=2)
-    upper_work_hours = models.IntegerField()
-    lower_work_hours = models.IntegerField()
-    assigned_job = models.CharField(max_length=50,blank=True)
-    summary = models.CharField(max_length=100,blank=True)
+    project_name = models.CharField(max_length=100, verbose_name='プロジェクト名')
+    start_month = models.CharField(max_length=2, verbose_name='開始月')
+    last_month = models.CharField(max_length=2, verbose_name='終了月')
+    upper_work_hours = models.IntegerField(verbose_name='稼働上限時間')
+    lower_work_hours = models.IntegerField(verbose_name='稼働下限時間')
+    assigned_job = models.CharField(max_length=50,blank=True, verbose_name='担当業務')
+    summary = models.CharField(max_length=100,blank=True, verbose_name='概要')
     user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='ユーザー')
     company = models.ForeignKey(ContractCompanies, on_delete=models.PROTECT, verbose_name='契約会社')
 
 
 class WorkPattern(models.Model):
-    start_time = models.TimeField()
-    close_time = models.TimeField()
-    start_break_time = models.TimeField()
-    close_break_time = models.TimeField()
-    start_overtime_break = models.TimeField()
-    close_overtime_break = models.TimeField()
-    round_down_unit = models.IntegerField()
+    start_time = models.TimeField(verbose_name='勤務開始時刻')
+    close_time = models.TimeField(verbose_name='勤務終了時刻')
+    start_break_time = models.TimeField(verbose_name='休憩開始時刻')
+    close_break_time = models.TimeField(verbose_name='休憩終了時刻')
+    start_overtime_break = models.TimeField(verbose_name='時間外休憩開始時刻')
+    close_overtime_break = models.TimeField(verbose_name='時間外休憩終了時刻')
+    round_down_unit = models.IntegerField(verbose_name='時間切捨単位')
 
 
 class Attendance(models.Model):
-    date = models.DateTimeField()
-    start_time = models.TimeField()
-    close_time = models.TimeField()
+    date = models.DateTimeField(verbose_name='年月日')
+    start_time = models.TimeField(verbose_name='勤務開始時刻')
+    close_time = models.TimeField(verbose_name='勤務終了時刻')
     user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='ユーザー')
     project = models.ForeignKey(Projects, on_delete=models.PROTECT, verbose_name='プロジェクト')
     work_pattern = models.ForeignKey(WorkPattern, on_delete=models.PROTECT, verbose_name='勤務パターン')
