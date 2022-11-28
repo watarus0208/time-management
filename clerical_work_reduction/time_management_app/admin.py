6088dcbeb6f3cd9dc2a306da11e50781c93ba8e8
杉本亙 <sugimotowataru@sugimotowatarunoMacBook-Air.local>
2022-11-28 12:05:32 +0900

    管理画面モデル反映

--- clerical_work_reduction/time_management_app/admin.py
+++ clerical_work_reduction/time_management_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Users)
+admin.site.register(models.ContractCompanies)
+admin.site.register(models.SalesPeople)
+admin.site.register(models.Projects)
+admin.site.register(models.Attendance)
+admin.site.register(models.WorkPattern)
