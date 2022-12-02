984c36b761ca16c9999906acf082a07a04356f16
杉本亙 <sugimotowataru@sugimotowatarunoMacBook-Air.local>
2022-12-02 14:57:39 +0900

    月次ページへカレンダー情報を送信

--- clerical_work_reduction/time_management_app/mixins.py
+++ clerical_work_reduction/time_management_app/mixins.py
@@ -1,5 +1,8 @@
 import calendar
 import datetime
+from . import models
+from django.views import generic
+from collections import defaultdict
 
 class BaseCalendarMixin:
     """カレンダー関連Mixinの基底クラス"""
@@ -9,7 +12,25 @@ class BaseCalendarMixin:
     first_weekday = calendar.weekday(today.year, today.month, 1)
 
 
-class MonthCalendarMixin(BaseCalendarMixin):
+class MonthCalendarMixin(BaseCalendarMixin, generic.TemplateView):
     def get_timedelta(self, start_time, close_time):
         tdelta = datetime.datetime.combine(self.today, close_time) - datetime.datetime.combine(self.today, start_time)
         return tdelta
+    
+    def get_calendar_data(self, **kwargs):
+        attendances = models.Attendance.objects.all() 
+        calendar_data = defaultdict(dict)
+        for d in range(self.month_days):
+            calendar_data[d+1]['weekday'] = self.weekday[(d+self.first_weekday)%7]
+        for a in attendances:
+            calendar_data[a.date.day]['start_time'] = a.start_time
+            calendar_data[a.date.day]['close_time'] = a.close_time
+            calendar_data[a.date.day]['attend_time'] = self.get_timedelta(a.start_time, a.close_time)
+            calendar_data[a.date.day]['work_time'] = self.get_timedelta(a.start_time, a.close_time) - self.get_timedelta(a.work_pattern.start_break_time, a.work_pattern.close_break_time) 
+            calendar_data[a.date.day]['break_time'] = self.get_timedelta(a.work_pattern.start_break_time, a.work_pattern.close_break_time)
+            calendar_data[a.date.day]['overtime_break_time'] = self.get_timedelta(a.work_pattern.start_break_time, a.work_pattern.close_break_time) 
+            calendar_data[a.date.day]['today_todo'] = a.today_todo
+            calendar_data[a.date.day]['today_issue'] = a.today_issue
+            calendar_data[a.date.day]['work_pattern'] = a.work_pattern
+        calendar = {'calendar_data': dict(calendar_data)} 
+        return calendar
--- clerical_work_reduction/time_management_app/views.py
+++ clerical_work_reduction/time_management_app/views.py
@@ -37,25 +37,12 @@ class HomeView(LoginRequiredMixin, TemplateView):
 class MonthView(mixins.MonthCalendarMixin, LoginRequiredMixin, TemplateView):
     template_name = 'time_management_app/month.html'
 
-    def get(self, request, **kwargs):
-        attendances = Attendance.objects.all() 
-        
-        month_data = defaultdict(dict)
-        for d in range(self.month_days):
-            month_data[d+1]['weekday'] = self.weekday[(d+self.first_weekday)%7]
-        for a in attendances:
-            month_data[a.date.day]['start_time'] = a.start_time
-            month_data[a.date.day]['close_time'] = a.close_time
-            month_data[a.date.day]['attend_time'] = self.get_timedelta(a.start_time, a.close_time)
-            month_data[a.date.day]['work_time'] = self.get_timedelta(a.start_time, a.close_time) - self.get_timedelta(a.work_pattern.start_break_time, a.work_pattern.close_break_time) 
-            month_data[a.date.day]['break_time'] = self.get_timedelta(a.work_pattern.start_break_time, a.work_pattern.close_break_time)
-            month_data[a.date.day]['overtime_break_time'] = self.get_timedelta(a.work_pattern.start_break_time, a.work_pattern.close_break_time) 
-            month_data[a.date.day]['today_todo'] = a.today_todo
-            month_data[a.date.day]['today_issue'] = a.today_issue
-            month_data[a.date.day]['work_pattern'] = a.work_pattern
-        print(month_data)
-
-        return render(request, 'time_management_app/month.html', month_data)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_calendar_data()
+        context.update(calendar_context)
+        print(context)
+        return context
 
 
 class ConfigView(LoginRequiredMixin, TemplateView):
